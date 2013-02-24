d3e99ef5641f91543902ae73848d2d79f93e17bd
yoshinbo <yoshinbo@gmail.com>
2013-02-24 11:44:55 +0900

    init install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eedrops::Application.config.secret_token = '092dc05d6acf2663854b2f0a6fb8ee67038e08af42d3d24eb33b50f5f7231af4a3f9dbf49a0af6592f04a1b52f732215220e3f3fa863dec5f86640319cf95051'
